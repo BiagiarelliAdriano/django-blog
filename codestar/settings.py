01057ff0952168edb379ab5903340e4a6b91660f
Adriano Biagiarelli <adrianobiagiarelli@gmail.com>
2024-11-05 10:36:00 +0000

    Add files for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mnu_ni2k5z$@7de2vy98+_n$9v+=wq*$)o&3df5@eagnfl@bcm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-biagiarellia-djangoblog-3f6rxfzpov7.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-biagiarellia-djangoblog-3f6rxfzpov7.ws.codeinstitute-ide.net',
+                '.herokuapp.com']
 
 
 # Application definition
